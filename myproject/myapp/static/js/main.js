a631a00d8037de17496022ec6e524c0e2eacfdef
Reon Augustine <augustinereon@gmail.com>
2024-04-21 15:20:17 +0530

    interactivity-done

--- myproject/myapp/static/js/main.js
+++ myproject/myapp/static/js/main.js
@@ -2,47 +2,22 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-// Create the scene and camera
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xdddddd); // Set background color to light gray
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.aspect = window.innerWidth / (window.innerHeight * 0.8); // Decrease the height of the scene
-camera.updateProjectionMatrix();
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+let object;
+let controls;
 
-// Create the renderer and attach it to the DOM
-const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("container3D").appendChild(renderer.domElement);
-
-// Set the initial camera position
-camera.position.z = 5;
-
-// Load and add lighting
-const mainLight = new THREE.DirectionalLight(0xffffff, 2); // Increased intensity
-mainLight.position.set(0, 1, 1); // Set position of main light source
-scene.add(mainLight);
-
-const secondaryLight = new THREE.DirectionalLight(0xffffff, 1); // Increased intensity
-secondaryLight.position.set(0, -1, -1); // Set position of secondary light source
-scene.add(secondaryLight);
-
-const additionalLight = new THREE.DirectionalLight(0xffffff, 1); // New light
-additionalLight.position.set(1, 0, 0); // Set position of additional light source
-scene.add(additionalLight);
-
-// Add ambient light to illuminate all objects equally
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
-scene.add(ambientLight);
-
-// Initialize GLTFLoader
 const loader = new GLTFLoader();
 
-// Load the GLB file
 loader.load(
     '/static/0/mesh.glb',
     function (gltf) {
         // Add the loaded object to the scene
-        scene.add(gltf.scene);
+        object = gltf.scene; // Set the object variable to the loaded GLTF object
+        scene.add(object);
     },
     function (xhr) {
         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -52,22 +27,158 @@ loader.load(
     }
 );
 
-// Initialize OrbitControls for camera interaction
-const controls = new OrbitControls(camera, renderer.domElement);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight*0.7);
+document.getElementById("container3D").appendChild(renderer.domElement);
 
-// Animation loop
+camera.position.z = 30;
+
+// Directional light
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 800, 500);
+topLight.castShadow = true;
+scene.add(topLight);
+
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0x333333, 5);
+scene.add(ambientLight);
+
+// Point lights to illuminate the scene
+const pointLight1 = new THREE.PointLight(0xffffff, 0.5);
+pointLight1.position.set(0, 0, 50);
+scene.add(pointLight1);
+
+const pointLight2 = new THREE.PointLight(0xffffff, 0.5);
+pointLight2.position.set(0, 0, -50);
+scene.add(pointLight2);
+
+const pointLight3 = new THREE.PointLight(0xffffff, 0.5);
+pointLight3.position.set(50, 0, 0);
+scene.add(pointLight3);
+
+const pointLight4 = new THREE.PointLight(0xffffff, 0.5);
+pointLight4.position.set(-50, 0, 0);
+scene.add(pointLight4);
+
+controls = new OrbitControls(camera, renderer.domElement);
+
+let shouldInteract = false;
+
+const interactButton = document.getElementById("interactButton");
+interactButton.addEventListener("click", function () {
+    shouldInteract = !shouldInteract;
+    if (!shouldInteract) {
+        resetModelPosition();
+    }
+});
+
+function resetModelPosition() {
+    object.rotation.set(0, 0, 0);
+}
+
 function animate() {
     requestAnimationFrame(animate);
-    controls.update(); // Update controls
+
+    if (shouldInteract) {
+        const containerRect = document.getElementById("container3D").getBoundingClientRect();
+        const containerWidth = containerRect.width;
+        const containerHeight = containerRect.height;
+        const isMouseInsideContainer = mouseX >= containerRect.left && mouseX <= containerRect.right &&
+                                       mouseY >= containerRect.top && mouseY <= containerRect.bottom;
+
+        if (isMouseInsideContainer) {
+            const mouseXInContainer = mouseX - containerRect.left;
+            const mouseYInContainer = mouseY - containerRect.top;
+
+            object.rotation.y = -3 + (mouseXInContainer / containerWidth) * 3;
+            object.rotation.x = -1.2 + (mouseYInContainer / containerHeight) * 2.5;
+        }
+    }
+
+    controls.update();
     renderer.render(scene, camera);
 }
 
-// Window resize event listener
-window.addEventListener("resize", function() {
-    camera.aspect = window.innerWidth / (window.innerHeight * 0.8); // Decrease the height of the scene
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-// Start the animation loop
+const container = document.getElementById("container3D");
+container.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX - container.getBoundingClientRect().left;
+    mouseY = e.clientY - container.getBoundingClientRect().top;
+});
+
 animate();
+
+// Panning Button Functionality
+const panButton = document.getElementById("panButton");
+let isPanning = false;
+
+panButton.addEventListener("click", function () {
+    isPanning = !isPanning;
+    if (isPanning) {
+        enablePanning();
+    } else {
+        disablePanning();
+    }
+});
+
+function enablePanning() {
+    controls.enableRotate = false;
+    controls.enablePan = true;
+}
+
+function disablePanning() {
+    controls.enableRotate = true;
+    controls.enablePan = false;
+}
+
+
+let isFullScreen = false;
+
+const fullscreenButton = document.getElementById("fullscreenButton");
+const exitFullscreenButton = document.getElementById("exitFullscreenButton");
+const backButton = document.getElementById("backButton");
+
+fullscreenButton.addEventListener("click", toggleFullScreen);
+exitFullscreenButton.addEventListener("click", exitFullScreen);
+
+function toggleFullScreen() {
+    const container = document.querySelector(".container");
+
+    if (!document.fullscreenElement) {
+        if (container.requestFullscreen) {
+            container.requestFullscreen();
+        } else if (container.webkitRequestFullscreen) {
+            container.webkitRequestFullscreen();
+        } else if (container.msRequestFullscreen) {
+            container.msRequestFullscreen();
+        }
+        isFullScreen = true;
+        exitFullscreenButton.style.display = "inline-block";
+        fullscreenButton.style.display = "none";
+        backButton.style.display = "inline-block";
+    }
+}
+
+function exitFullScreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+    isFullScreen = false;
+    exitFullscreenButton.style.display = "none";
+    fullscreenButton.style.display = "inline-block";
+    backButton.style.display = "none";
+}
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape" && isFullScreen) {
+        exitFullScreen();
+    }
+});
